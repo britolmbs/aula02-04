a0fabfef25ec835c660c7cd3fcc026c405e5b278
Lucas Brito <britolmbs@hotmail.com>
2018-04-02 21:11:27 -0300

    primeiro commit

---
+++ src/app/domain/endereco.ts
@@ -0,0 +1,11 @@
+export interface Endereco{
+    cep : string;
+    logradouro: string;
+    complemento: string;
+    bairro : string;
+    localidade: string;
+    uf: string;
+    unidade: string;
+    idge: string;
+    gia: string;
+}
